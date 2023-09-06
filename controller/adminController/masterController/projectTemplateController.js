201dae8dd7715bb34891f289b2e0b3b070786528
jayeshcloud202 <jayesh.m@cloud202.com>
2023-09-06 11:01:37 +0530

    created storeTemplate Api for project template

--- controller/adminController/masterController/projectTemplateController.js
+++ controller/adminController/masterController/projectTemplateController.js
@@ -3,7 +3,20 @@ const ProjectTemplate = require('../../../models/admin/master/projectTemplate');
 
 const projectTemplateController = {
     async storeTemplate(req, res, next) {
-    
+        try {
+            const { error } = masterTemplateSchema.validate(req.body);
+            if (error) {
+                return next(error);
+            }
+            const newTemplate = new ProjectTemplate({ ...req.body });
+            const lastSavedTemplate = await ProjectTemplate.findOne().sort({ createdAt: -1 });
+            let newProjectId = lastSavedTemplate ? parseInt(lastSavedTemplate.project_id.slice(6)) + 1 : 1;
+            newTemplate.project_id = 'QC_' + newProjectId;
+            const savedTemplate = await newTemplate.save();
+            return res.status(201).send(savedTemplate);
+        } catch (error) {
+            return next(error);
+        }
     },
 };
 
